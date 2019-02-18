f9769b48857370abe2afacab00dd60989576aaf5
Ben Borchard <ben.borchard@gmail.com>
2019-02-18 18:05:17 -0500

    Minor producation edits
    - Changed webapp title to 'Set' from 'React App'
    - Change icon to a set icon
    - Edited dynamic grid display to help with phone display

--- frontend/setwebapp/src/components/Board.js
+++ frontend/setwebapp/src/components/Board.js
@@ -8,9 +8,11 @@ class Board extends Component {
 
   render() {
     // dynamic rows - auto size to fit in screen
+    const numRows = this.props.cards.length / 3;
+    const rowHeightPct = 100 / numRows;
     let gridTemplateRows = "";
-    for (var i = 0; i < this.props.cards.length / 3; i++) {
-      gridTemplateRows += "auto "
+    for (var i = 0; i < numRows; i++) {
+      gridTemplateRows += (rowHeightPct + "% ");
     }
     return (
       <div class="setapp-game-board-grid" style={{"grid-template-rows": gridTemplateRows}}>
